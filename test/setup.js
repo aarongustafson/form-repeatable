7616bb26b093213fdcd536c431026c1267556e68
Aaron Gustafson <aaron@easy-designs.net>
2025-12-01 15:53:39 -0800

    Mock internals setup for tests

--- test/setup.js
+++ test/setup.js
@@ -1,8 +1,27 @@
 import { beforeAll } from 'vitest';
 import { FormRepeatableElement } from '../form-repeatable.js';
 
-// Define the custom element before tests run
+// Mock ElementInternals for testing environment
 beforeAll(() => {
+	// Mock attachInternals for Happy-DOM (doesn't support it natively)
+	if (!HTMLElement.prototype.attachInternals) {
+		HTMLElement.prototype.attachInternals = function () {
+			return {
+				setFormValue: () => {},
+				setValidity: () => {},
+				checkValidity: () => true,
+				reportValidity: () => true,
+				validationMessage: '',
+				validity: {
+					valid: true,
+				},
+				willValidate: true,
+				form: null,
+				labels: [],
+			};
+		};
+	}
+
 	if (!customElements.get('form-repeatable')) {
 		customElements.define('form-repeatable', FormRepeatableElement);
 	}
