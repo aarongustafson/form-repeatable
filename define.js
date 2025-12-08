e6a4bd18cef1ca6f4fe791a4607f85ae11414985
Aaron Gustafson <aaron@easy-designs.net>
2025-12-07 20:50:31 -0800

    Harden define

--- define.js
+++ define.js
@@ -1,3 +1,18 @@
 import { FormRepeatableElement } from './form-repeatable.js';
 
-customElements.define('form-repeatable', FormRepeatableElement);
+export function defineFormRepeatable(tagName = 'form-repeatable') {
+	const hasWindow = typeof window !== 'undefined';
+	const registry = hasWindow ? window.customElements : undefined;
+
+	if (!registry || typeof registry.define !== 'function') {
+		return false;
+	}
+
+	if (!registry.get(tagName)) {
+		registry.define(tagName, FormRepeatableElement);
+	}
+
+	return true;
+}
+
+defineFormRepeatable();
